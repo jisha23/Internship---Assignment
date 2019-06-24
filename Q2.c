a4b14086585bf7835872c2b342e244910dd33b69
jisha23 <37058304+jisha23@users.noreply.github.com>
2019-06-25 02:04:27 +0530

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,j,c=0,arr[100],k=0;
+    printf("\nEnter the lower limit ");
+    scanf("%d",&a);
+    printf("\nEnter the upper limit ");
+    scanf("%d",&b);
+    for(i=a;i<=b;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                c++;
+            }
+        }
+        if(c==2)
+        {
+            arr[k++]=i;
+        }
+        c=0;
+    }
+    if(k==0)
+    {
+        printf("\nThere are no prime numbers");
+    }
+    else
+    {
+        printf("\nThe prime numbers are: ");
+        for(i=0;i<k;i++)
+        {
+            printf(" %d",arr[i]);
+        }
+    }
+    
+}
